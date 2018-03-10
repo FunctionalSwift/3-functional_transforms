2d751fc83a870063ebc99683405a397d0edb48f4
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-10 18:31:16 +0100

    Imperative style again... Are you sure?

--- Functional transforms.playground/Contents.swift
+++ Functional transforms.playground/Contents.swift
@@ -26,3 +26,37 @@ var databases: [JsonArray] {
 }
 
 print(databases)
+
+var userDatabase = JsonArray()
+
+for db in databases {
+    userDatabase.append(contentsOf: db)
+}
+
+var hosts = [String]()
+
+for user in userDatabase {
+    if let email = user["email"] as? String,
+        let host = email.components(separatedBy: "@").last,
+        !hosts.contains(host) {
+        
+        hosts.append(host)
+    }
+}
+
+func hostInfo(db: JsonArray, host: String) -> (Int, Int) {
+    var count = 0
+    var age = 0
+    
+    for user in db {
+        if let email = user["email"] as? String,
+            let userHost = email.components(separatedBy: "@").last,
+            let userAge = user["age"] as? Int, userHost == host {
+            
+            count += 1
+            age += userAge
+        }
+    }
+    
+    return (count, age / count)
+}
