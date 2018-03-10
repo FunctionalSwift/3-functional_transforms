b626301239c9f6c763bcc20fae1c5e68c51d24b4
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-10 18:38:05 +0100

    mmm wait!! mutable state everywhere!!

--- Functional transforms.playground/Contents.swift
+++ Functional transforms.playground/Contents.swift
@@ -59,3 +59,15 @@ func hostInfo(db: JsonArray, host: String) -> HostInfo {
     
     return HostInfo(count: count, age: age / count)
 }
+
+var hostsInfo = [HostInfo]()
+
+for host in hosts {
+    hostsInfo.append(hostInfo(db: userDatabase, host: host))
+}
+
+for i in 0..<hosts.count {
+    print("Host: \(hosts[i])")
+    print("  - Count: \(hostsInfo[i].count) users")
+    print("  - Average age: \(hostsInfo[i].age) years old")
+}
