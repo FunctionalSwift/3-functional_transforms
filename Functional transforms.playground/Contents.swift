7a30fb5b5ee9c4ed90508448ec1a119c069c06d3
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-10 18:27:26 +0100

    DB connection framework functions

--- Functional transforms.playground/Contents.swift
+++ Functional transforms.playground/Contents.swift
@@ -1,1 +1,28 @@
 //: Playground - Functional transforms
+
+import Foundation
+
+public typealias JsonObject = [String: AnyObject]
+public typealias JsonArray = [JsonObject]
+
+func json(path: String) -> JsonArray {
+    guard
+        let path = Bundle.main.path(forResource: path, ofType: "json"),
+        let jsonData = NSData(contentsOfFile: path),
+        let jsonResult = try! JSONSerialization.jsonObject(with: jsonData as Data) as? NSArray
+        else {
+            return JsonArray()
+    }
+    
+    return jsonResult.flatMap { $0 as? JsonObject }
+}
+
+var databases: [JsonArray] {
+    return [
+        json(path: "Database1"),
+        json(path: "Database2"),
+        json(path: "Database3")
+    ]
+}
+
+print(databases)
